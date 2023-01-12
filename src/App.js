63a03f24c58c6a4cce962537a6f343cd5db7c7ba
Thazin Moe <thazinmoe1097@gmail.com>
2023-01-12 10:47:10 +0630

    createContext

--- src/App.js
+++ src/App.js
@@ -1,124 +1,25 @@
-import React, { useState } from 'react';
-import {Task} from './Task';
-import './App.css';
+import React, { useState } from "react";
+import Task from "./Task";
+import "./App.css";
+import { TodosContext } from "./context/TodosContext";
+import TodoForm from "./TodoForm";
 
 function App() {
-  const [todos, setTodo] = useState([]);
-  const [todoInput, setTodoInput] = useState("");
-
-  function handleInput(e) {
-    setTodoInput(e.target.value);
-    console.log("setTodoInput");
-  }
-
-  function addTodo(e) {
-    e.preventDefault();
-    if(todoInput.trim().length === 0){
-      return;
-    }
-    const todoList = {
-      id: todos.length + 1,
-      title: todoInput,
-      isComplete: false
-    };
-
-    setTodo([...todos, todoList]);
-    console.log("todos", todos);
-
-    setTodoInput('')
-  }
-
-  const deleteTodo = (id) => {
-    console.log("id", id)
-    setTodo(todos.filter((tododelete) => tododelete.id !== id))
-  }
-
-  function completeTodo(id) {
-
-    const updateTodos = todos.map(todo => {
-      if (todo.id === id) {
-        todo.isComplete = !todo.isComplete;
-      }
-      return todo;
-    });
-
-    console.log("completeTodo", updateTodos);
-    setTodo(updateTodos);
-  }
-
-  // const handleClick = event => {
-  //   if(event.target.style.textDecoration){
-  //     event.target.style.removeProperty('text-decoration');
-  //   } else{
-  //     event.target.style.setProperty('text-decoration', 'line-through')
-  //   }
-  // }
-
-  function markAsEditing(id) {
-    const updateTodos = todos.map(todo => {
-      if (todo.id === id) {
-        todo.isEditing = true;
-      }
-      return todo;
-    });
-    setTodo(updateTodos)
-  }
-
-  function updateTodo(event, id) {
-    const updateTodos = todos.map(todo => {
-      if (todo.id === id) {
-        if (event.target.value.trim().length === 0) {
-          todo.isEditing = false;
-          return todo;
-        }
-        todo.title = event.target.value;
-        todo.isEditing = false;
-      }
-      return todo;
-    });
-    setTodo(updateTodos);
-  }
-
-  function cancelEdit(event,id){
-    const updateTodos = todos.map(todo => {
-      if(todo.id === id){
-        todo.isEditing = false;
-      }
-      return todo;
-    });
-    console.log("cancel");
-    setTodo(updateTodos);
-  }
-
+  const [ todos, setTodo ] = useState([]);
+  const [ todoInput, setTodoInput ] = useState("");
 
   return (
-    <div>
-      <input type="text"
-        value={todoInput}
-        onChange={handleInput}
-        placeholder="What do you need to do?"
-      />
-      <button onClick={addTodo}>Add Todo</button>
-      <ul>
-        {todos.map((todo,index) => {
-          return (
-            <React.Fragment key={index}>
-              <Task
-                id = {todo.id}
-                title = {todo.title}
-                isComplete = {todo.isComplete}
-                isEditing = {todo.isEditing}
-                completeTodo = {completeTodo}
-                markAsEditing = {markAsEditing}
-                updateTodo = {updateTodo}
-                cancelEdit = {cancelEdit}
-                deleteTodo = {deleteTodo}
-              />
-            </React.Fragment>
-          );
-        })}
-      </ul>
-    </div>
+    <TodosContext.Provider
+      value={{
+        todos,
+        setTodo,
+        todoInput,
+        setTodoInput,
+      }}
+    >
+      <TodoForm />
+      {todos.length >0 ? <Task /> : ''}
+    </TodosContext.Provider>
   );
 }
 
--- src/Task.js
+++ src/Task.js
@@ -1,36 +1,97 @@
-export const Task = (props) => {
-    return (
-        <li>
-            {!props.isEditing ? (
-                <span>
-                    <input
-                        type="checkbox"
-                        onChange={() => props.completeTodo(props.id)}
-                        checked={props.isComplete ? true : false} />
-                    <span
-                        onDoubleClick={() => props.markAsEditing(props.id)}
-                        style={{ textDecoration: props.isComplete ? 'line-through' : '' }}
-                    >
-                        {props.title}
-                    </span>
-                </span>
-            ) : (
-                <input
-                    type="text"
-                    onBlur={(event) => props.updateTodo(event, props.id)}
-                    onKeyDown={event => {
-                        if (event.key === 'Enter') {
-                            props.updateTodo(event, props.id);
-                        } else if (event.key === 'Escape') {
-                            props.cancelEdit(event, props.id);
-                        }
-                    }}
-                    defaultValue={props.title}
-                    autoFocus
-                />
-            )}
+import React, { useContext } from "react";
+import { TodosContext } from "./context/TodosContext";
 
-            <button onClick={() => props.deleteTodo(props.id)}>X</button>
-        </li>
-    );
+export const Task = () => {
+  const { todos, setTodo } = useContext(TodosContext);
+
+  function completeTodo(id) {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodo(updateTodos);
+  }
+
+  function markAsEditing(id) {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isEditing = true;
+      }
+      return todo;
+    });
+    setTodo(updateTodos);
+  }
+
+  function updateTodo(event, id){
+    const updateTodos = todos.map((todo) => {
+        if(todo.id === id){
+           if(event.target.value.trim().length === 0){
+            todo.isEditing = false;
+            return todo;
+           } 
+           todo.title = event.target.value;
+           todo.isEditing = false;
+        }
+        return todo;
+    });
+    setTodo(updateTodos);
+  }
+
+  function cancelTodo(event, id){
+    const updateTodos = todos.map(todo => {
+        if(todo.id === id){
+            todo.isEditing = false;
+        }
+        return todo;
+    });
+    setTodo(updateTodos);
+  }
+
+  function deleteTodo(id) {
+    setTodo([...todos].filter((todo) => todo.id !== id));
+    console.log("delete", id);
+  }
+  return (
+    <ul>
+      {todos.map((todo, i) => {
+        return (
+          <li key={i}>
+            <input
+              type="checkbox"
+              onChange={() => completeTodo(todo.id)}
+              checked={todo.isComplete ? true : false}
+            />
+
+            {!todo.isEditing ? (
+              <span
+                onDoubleClick={() => markAsEditing(todo.id)}
+                style={{ textDecoration: todo.isComplete ? 'line-through' : 'none' }}
+              >
+                {todo.title}
+              </span>
+            ) : (
+              <input
+                type="text" 
+                onBlur={(event) => updateTodo(event, todo.id)}
+                onKeyDown={(event) => {
+                    if(event.key === 'Enter'){
+                        updateTodo(event,todo.id)
+                    }else if(event.key === 'Escape'){
+                        cancelTodo(event, todo.id)
+                    }
+                }}
+                defaultValue={todo.title}
+                autoFocus
+              />
+            )}
+            <button onClick={() => deleteTodo(todo.id)}>X</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
+
+export default Task;
